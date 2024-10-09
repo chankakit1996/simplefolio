84dcde566f57e113333c5d27b537ab4b74890bfe
Ken <chankakit1996@yahoo.com.hk>
2024-10-09 16:41:10 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 yarn build
 git add dist -f
-git add .
 git commit -m "deploy"
-git subtree push --prefix dist origin gh-pages -f
+git push origin `git subtree split --prefix dist origin`:gh-pages --force
