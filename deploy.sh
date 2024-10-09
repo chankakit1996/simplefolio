888ae11a77cb9e7fd06aec46a3e28113eb47264f
Ken <chankakit1996@yahoo.com.hk>
2024-10-09 16:30:32 +0800

    fix deploy script, change to yarn

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
-npm ci
-npm run build
+yarn build
 git add dist -f
 git add .
 git commit -m "deploy"
-git subtree push --prefix dist origin gh-pages
+git subtree push --prefix dist origin gh-pages -f
